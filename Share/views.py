7bef003921d54acd5944dd2a617d569a19a8855b
fengchunsgit <1055688803@qq.com>
2017-12-26 19:43:08 +0800

    Post

--- Share/views.py
+++ Share/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
-from  .models import Upload
-from django.http import HttpResponseRedirect
+from .models import Upload
+from django.http import HttpResponsePermanentRedirect
 import random
 import string
-import  datetime
+#import  datetime
 
 # Create your views here.
 
@@ -12,23 +12,23 @@ class HomeView(View):
     def get(self,request):
         return render(request,"base.html",{})
 
-    def post (self, request): #post request
+    def post(self, request):
         if request.FILES:
             file = request.FILES.get("file")
             name = file.name
             size = int(file.size)
-            with open('static/file/'+name, 'wb') as f:
+            with open('static/file/' + name, 'wb')as f:
                 f.write(file.read())
-            code = '' . join(random.sample(string.digits, 8))
+            code = ''.join(random.sample(string.digits, 8))
             u = Upload(
-                path='static/file/'+name,
+                path='static/file/' + name,
                 name=name,
                 Filesize=size,
                 code=code,
                 PCIP=str(request.META['REMOTE_ADDR']),
             )
             u.save()
-            return HttpResponseRedirect("/s/"+code)
+            return HttpResponsePermanentRedirect("/s/" + code)
 
 
 class DisplayView(View): #展示文件视图类
