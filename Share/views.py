bbfc8a24efdb2f4d296e8b7fd4c2c48cddf9b1f5
fengchunsgit <1055688803@qq.com>
2017-12-26 19:39:20 +0800

    HomeView post

--- Share/views.py
+++ Share/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 from django.views.generic import View
+from  .models import Upload
+from django.http import HttpResponseRedirect
+import random
+import string
+import  datetime
 
 # Create your views here.
 
 class HomeView(View):
     def get(self,request):
         return render(request,"base.html",{})
+
+    def post (self, request): #post request
+        if request.FILES:
+            file = request.FILES.get("file")
+            name = file.name
+            size = int(file.size)
+            with open('static/file/'+name, 'wb') as f:
+                f.write(file.read())
+            code = '' . join(random.sample(string.digits, 8))
+            u = Upload(
+                path='static/file/'+name,
+                name=name,
+                Filesize=size,
+                code=code,
+                PCIP=str(request.META['REMOTE_ADDR']),
+            )
+            u.save()
+            return HttpResponseRedirect("/s/"+code)
+
+
+class DisplayView(View): #展示文件视图类
+    def get(self,request,code):
+        u=Upload.objects.filter(code=str(code))
+
+        if u:#如果有内容，u的访问次数+1,否则返回给前端的内容也是空的
+            for i in u:
+                i.DownloadDoccount+=1 #每次访问，访问次数加1
+                i.save()
+        return render(request,'content.html',{"content":u})
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from Share.views import HomeView
+from Share.views import HomeView,DisplayView
 
 urlpatterns = [
     # Examples:
@@ -9,4 +9,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', HomeView.as_view(),name="home"),
+    url(r'^s/(?P<code>\d+)/$', HomeView.as_view(),name="home"),#/s/(?P<code>\d+ 使用了组匹配的方式 匹配code任意长度的数字，如/s/123456，将123456传给 DisplayView,这里的 code 必须和视图函数的 code 保持一致。
+
+
 ]
